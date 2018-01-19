3c8ccbca80d3abfa397207ab7c14e36686f3cb75
Erik Shafer <erik.shafer@gmail.com>
2018-01-19 15:38:22 -0600

    Half way through last module. Grapes issue on MacBook Pro. :(

--- gps-project/src/ImportGpsData.groovy
+++ gps-project/src/ImportGpsData.groovy
@@ -1,29 +1,26 @@
-def file = new File('../data/fells_loop.gpx')
-println file.exists()
+@Grapes(
+    @Grab(group='joda-time', module='joda-time', version='2.9.9')
+)
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat 
 
+def file = new File('../data/fells_loop.gpx')
 def slurper = new XmlSlurper()
 def gpx = slurper.parse(file)
 
-// There is no method called name or desc, but due to the dynamic
-// nature of Groovy, at run-time it will try to resolve them. It
-// finds the properties and prints them accordingly.
-println gpx.name            // XML element, no special syntax
-println ''
-println gpx.desc            // XML element
-println ''
-// println gpx.foo // There is no 'foo' in the XML. No error is printed.
-println gpx.@version        // @ is 'attribute notation'
-println gpx.@creator        // XML attribute
+println gpx.name 
+println gpx.desc
 
-// print out latitude and long, both are XML attributes
-// for (point in gpx.rte.rtept) {
-//     println point.@lat      
-//     println point.@lon      
-//     println point.time 
-// }
+println gpx.@version
+println gpx.@creator
 
 gpx.rte.rtept.each {
     println it.@lat
     println it.@lon
-    println it.time 
+    
+    // Created from the raw DateTime from Joda.
+    // Format doesn't include sec or ms. hh = 12 hour.
+    def printableTime = new DateTime(it.time.toString())
+    def format = DateTimeFormat.forPattern('MM/dd/yyyy - hh:mm aa')
+    println printableTime.toString(format)
 }
